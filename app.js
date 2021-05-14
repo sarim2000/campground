c5e02fc05c0529ae9b60c6d6d34d9766ec153b77
sarim2000 <sarimbleedblue@gmail.com>
2021-05-14 22:42:23 +0530

    latest commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
-
+const ejsMate = require("ejs-mate");
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
 	useNewUrlParser: true,
 	useCreateIndex: true,
@@ -20,7 +20,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
+app.engine("ejs", ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
