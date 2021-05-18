52115da23ef53c2826fa98fd3d4b22d9bd420640
sarim2000 <sarimbleedblue@gmail.com>
2021-05-18 17:33:33 +0530

    .

--- models/user.js
+++ models/user.js
@@ -1,14 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passportlocalmongoose = require("passport-local-mongoose");
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
 });
-UserSchema.plugin(passportlocalmongoose);
 
-module.exports = mongoose.model("User", UserSchema);
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
